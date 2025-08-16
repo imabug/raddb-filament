7d7ac7368514b857306cc026c3c9ae86b4516089
Eugene Mah <eugenemah@gmail.com>
2025-08-16 18:04:37 -0400

    Add monthly survey count widget

--- app/Filament/Widgets/MonthlySurveyCountWidget.php
+++ app/Filament/Widgets/MonthlySurveyCountWidget.php
@@ -11,6 +11,22 @@ class MonthlySurveyCountWidget extends ChartWidget
 {
     protected ?string $heading = 'Monthly Survey Count Widget';
     protected ?string $pollingInterval = null;
+    public ?string $filter = '';
+
+    protected function getFilters(): ?array
+    {
+        $years = TestDate::selectRaw('year(test_date) as years')
+                     ->distinct()
+                     ->orderBy('years','desc')
+                     ->get()
+                     ->all();
+
+        foreach ($years as $y) {
+            $yearFilter[$y['years']] = $y['years'];
+        }
+
+        return $yearFilter;
+    }
 
     public function getDescription(): ?string
     {
@@ -19,8 +35,26 @@ class MonthlySurveyCountWidget extends ChartWidget
 
     protected function getData(): array
     {
+        $activeFilter = empty($this->filter) ? date("Y") : $this->filter;
+
+        $monthlyCount = TestDate::selectRaw('count(test_date) as c')
+                            ->whereRaw('year(test_date)=?', $activeFilter)
+                            ->groupByRaw('month(test_date)')
+                            ->get()
+                            ->all();
+
+        foreach ($monthlyCount as $c) {
+            $chartData[] = $c['c'];
+        }
+
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Monthly survey counts',
+                    'data' => $chartData,
+                ],
+            ],
+            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         ];
     }
 

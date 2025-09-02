389a7d43e9a096de8df10cb82f6d1a701d43d8a7
Eugene Mah <eugenemah@gmail.com>
2025-09-02 18:58:53 -0400

    Resource for survey schedule view

---
+++ app/Filament/Raddb/Resources/SurveyScheduleViews/SurveyScheduleViewResource.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Filament\Raddb\Resources\SurveyScheduleViews;
+
+//use App\Filament\Raddb\Resources\SurveyScheduleViews\Pages\CreateSurveyScheduleView;
+//use App\Filament\Raddb\Resources\SurveyScheduleViews\Pages\EditSurveyScheduleView;
+use App\Filament\Raddb\Resources\SurveyScheduleViews\Pages\ListSurveyScheduleViews;
+use App\Filament\Raddb\Resources\SurveyScheduleViews\Schemas\SurveyScheduleViewForm;
+use App\Filament\Raddb\Resources\SurveyScheduleViews\Tables\SurveyScheduleViewsTable;
+use App\Models\SurveyScheduleView;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+
+class SurveyScheduleViewResource extends Resource
+{
+    protected static ?string $model = SurveyScheduleView::class;
+
+    protected static ?string $navigationLabel = "Survey Schedule";
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'description';
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->orderBy('prevSurveyDate', 'asc');
+    }
+
+    public static function form(Schema $schema): Schema
+    {
+        return SurveyScheduleViewForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return SurveyScheduleViewsTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListSurveyScheduleViews::route('/'),
+//            'create' => CreateSurveyScheduleView::route('/create'),
+//            'edit' => EditSurveyScheduleView::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Raddb/Resources/SurveyScheduleViews/Tables/SurveyScheduleViewsTable.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Raddb\Resources\SurveyScheduleViews\Tables;
+
+//use Filament\Actions\BulkActionGroup;
+//use Filament\Actions\DeleteBulkAction;
+//use Filament\Actions\EditAction;
+//use Filament\Support\Icons\Heroicon;
+use App\Models\SurveyScheduleView;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class SurveyScheduleViewsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id'),
+                TextColumn::make('description'),
+                TextColumn::make('prevSurveyId'),
+                TextColumn::make('prevSurveyDate')
+                    ->date('Y-m-d'),
+                TextColumn::make('currSurveyId'),
+                TextColumn::make('currSurveyDate')
+                    ->date('Y-m-d'),
+            ])
+            ->paginated(false);
+            // ->filters([
+            //     //
+            // ]);
+            // ->recordActions([
+            //     EditAction::make(),
+            // ])
+            // ->toolbarActions([
+            //     BulkActionGroup::make([
+            //         DeleteBulkAction::make(),
+            //     ]),
+            // ]);
+    }
+}

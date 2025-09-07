81b9b52da9c3f59f6d2f0134c98498f35299c9ee
Eugene Mah <eugenemah@gmail.com>
2025-09-07 07:59:00 -0400

    Change icon

--- app/Filament/Admin/Resources/Facilities/FacilityResource.php
+++ app/Filament/Admin/Resources/Facilities/FacilityResource.php
@@ -20,7 +20,7 @@ class FacilityResource extends Resource
 {
     protected static ?string $model = Facility::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedBuildingOffice2;
 
     protected static ?string $recordTitleAttribute = 'facility';
 

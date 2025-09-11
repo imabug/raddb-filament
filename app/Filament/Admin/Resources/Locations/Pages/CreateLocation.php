4d163be40099abd73bdecd67a952c23c63ed3a4b
Eugene Mah <eugenemah@gmail.com>
2025-09-11 10:37:40 -0400

    Keep last selected facility when creating a new record

--- app/Filament/Admin/Resources/Locations/Pages/CreateLocation.php
+++ app/Filament/Admin/Resources/Locations/Pages/CreateLocation.php
@@ -4,8 +4,14 @@ namespace App\Filament\Admin\Resources\Locations\Pages;
 
 use App\Filament\Admin\Resources\Locations\LocationResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Arr;
 
 class CreateLocation extends CreateRecord
 {
     protected static string $resource = LocationResource::class;
+
+    protected function preserveFormDataWhenCreatingAnother(array $data): array
+    {
+        return Arr::only($data, ['facility_id']);
+    }
 }

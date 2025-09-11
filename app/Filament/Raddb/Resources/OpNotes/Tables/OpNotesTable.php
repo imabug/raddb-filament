041ab888b9ea37a9befd7f77a699c88f8ecb5696
Eugene Mah <eugenemah@gmail.com>
2025-09-11 15:44:30 -0400

    Add resource for operational notes

---
+++ app/Filament/Raddb/Resources/OpNotes/Tables/OpNotesTable.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Filament\Raddb\Resources\OpNotes\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreBulkAction;
+use Filament\Actions\ViewAction;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Grouping\Group;
+use Filament\Tables\Table;
+
+class OpNotesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('note')
+                    ->searchable()
+            ])
+            ->groups([
+                Group::make('machine.description')
+                    ->collapsible(),
+            ])
+            ->defaultGroup('machine.description')
+            ->filters([
+                TrashedFilter::make(),
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+}

fe28ff75cfda63dc3e762a05928d45627248aa20
Eugene Mah <eugenemah@gmail.com>
2025-09-12 13:43:17 -0400

    Adjust indenting

--- app/Filament/Raddb/Resources/Machines/Tables/MachinesTable.php
+++ app/Filament/Raddb/Resources/Machines/Tables/MachinesTable.php
@@ -23,82 +23,82 @@ class MachinesTable
     public static function configure(Table $table): Table
     {
         return $table
-                   ->columns([
-                       IconColumn::make('machine_status')
-                           ->icon(fn (string $state): Heroicon => match ($state) {
-                               'Active' => Heroicon::Check,
-                               'Inactive' => Heroicon::XCircle,
-                               'Removed' => Heroicon::Trash,
-                           })
-                           ->color(fn (string $state): string => match ($state) {
-                               'Active' => 'success',
-                               'Inactive' => 'warning',
-                               'Removed' => 'danger',
-                               default => 'info',
-                           }),
-                       TextColumn::make('location.location')
-                           ->searchable(),
-                       TextColumn::make('modality.modality')
-                           ->searchable(),
-                       TextColumn::make('description')
-                           ->searchable(),
-                       TextColumn::make('manufacturer.manufacturer')
-                           ->searchable(),
-                       TextColumn::make('model')
-                           ->searchable(),
-                       TextColumn::make('serial_number')
-                           ->searchable(),
-                       TextColumn::make('vend_site_id')
-                           ->searchable(),
-                       TextColumn::make('room')
-                           ->searchable(),
-                       TextColumn::make('manuf_date')
-                           ->date('Y-m-d')
-                           ->sortable(),
-                       TextColumn::make('install_date')
-                           ->date('Y-m-d')
-                           ->sortable(),
-                       TextColumn::make('remove_date')
-                           ->date('Y-m-d')
-                           ->sortable(),
-                       TextColumn::make('age')
-                           ->sortable(),
-                   ])
-                   ->groups([
-                       Group::make('facility.facility')
-                           ->collapsible(),
-                       Group::make('location.location')
-                           ->collapsible(),
-                       Group::make('modality.modality')
-                           ->collapsible(),
-                       Group::make('manufacturer.manufacturer')
-                           ->collapsible(),
-                   ])
-                   ->defaultGroup('facility.facility')
-                   ->filters([
-                       TrashedFilter::make(),
-                       Filter::make('active')
-                           ->query(fn (Builder $query): Builder => $query->where('machine_status', "Active"))
-                           ->toggle()
-                           ->default(),
-                       SelectFilter::make('facility')
-                           ->relationship('facility', 'facility'),
-                       SelectFilter::make('modality')
-                           ->relationship('modality', 'modality'),
-                       SelectFilter::make('manufacturer')
-                           ->relationship('manufacturer', 'manufacturer'),
-                   ])
-                   ->deferFilters(false)
-                   ->recordActions([
-                           ViewAction::make(),
-                           EditAction::make(),
-                   ])
-                   ->toolbarActions([
-                           BulkActionGroup::make([
-                               DeleteBulkAction::make(),
-                               ForceDeleteBulkAction::make(),
-                               RestoreBulkAction::make(),
-                           ]),
-                   ]);
+            ->columns([
+                IconColumn::make('machine_status')
+                    ->icon(fn (string $state): Heroicon => match ($state) {
+                            'Active' => Heroicon::Check,
+                            'Inactive' => Heroicon::XCircle,
+                            'Removed' => Heroicon::Trash,
+                        })
+                    ->color(fn (string $state): string => match ($state) {
+                            'Active' => 'success',
+                            'Inactive' => 'warning',
+                            'Removed' => 'danger',
+                            default => 'info',
+                        }),
+                TextColumn::make('location.location')
+                    ->searchable(),
+                TextColumn::make('modality.modality')
+                    ->searchable(),
+                TextColumn::make('description')
+                    ->searchable(),
+                TextColumn::make('manufacturer.manufacturer')
+                    ->searchable(),
+                TextColumn::make('model')
+                    ->searchable(),
+                TextColumn::make('serial_number')
+                    ->searchable(),
+                TextColumn::make('vend_site_id')
+                    ->searchable(),
+                TextColumn::make('room')
+                    ->searchable(),
+                TextColumn::make('manuf_date')
+                    ->date('Y-m-d')
+                    ->sortable(),
+                TextColumn::make('install_date')
+                    ->date('Y-m-d')
+                    ->sortable(),
+                TextColumn::make('remove_date')
+                    ->date('Y-m-d')
+                    ->sortable(),
+                TextColumn::make('age')
+                    ->sortable(),
+            ])
+            ->groups([
+                Group::make('facility.facility')
+                    ->collapsible(),
+                Group::make('location.location')
+                    ->collapsible(),
+                Group::make('modality.modality')
+                    ->collapsible(),
+                Group::make('manufacturer.manufacturer')
+                    ->collapsible(),
+            ])
+            ->defaultGroup('facility.facility')
+            ->filters([
+                TrashedFilter::make(),
+                Filter::make('active')
+                    ->query(fn (Builder $query): Builder => $query->where('machine_status', "Active"))
+                    ->toggle()
+                    ->default(),
+                SelectFilter::make('facility')
+                    ->relationship('facility', 'facility'),
+                SelectFilter::make('modality')
+                    ->relationship('modality', 'modality'),
+                SelectFilter::make('manufacturer')
+                    ->relationship('manufacturer', 'manufacturer'),
+            ])
+            ->deferFilters(false)
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ]),
+            ]);
     }
 }

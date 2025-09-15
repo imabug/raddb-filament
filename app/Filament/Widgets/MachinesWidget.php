bd57737ba28e2f656905a8d7c7f444a0657cd7f6
Eugene Mah <eugenemah@gmail.com>
2025-09-15 13:39:24 -0400

    New Machines dashboard widget
    Pie chart widget for number of machines by modality.

---
+++ app/Filament/Widgets/MachinesWidget.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Machine;
+use App\Models\TestType;
+use Filament\Widgets\ChartWidget;
+
+class MachinesWidget extends ChartWidget
+{
+    protected ?string $heading = 'Machine Count by Modality';
+    protected ?string $pollingInterval = null;
+    protected static ?int $sort = 4;
+
+    public function getDescription(): ?string
+    {
+        return 'Count of active machines by modality.';
+    }
+
+    protected function getOptions(): array
+    {
+        return [
+            'plugins' => [
+                'colors' => [
+                    'enabled' => "true",
+                    'forceOverride' => "true",
+                ],
+            ],
+        ];
+    }
+    
+    protected function getData(): array
+    {
+        $machines = Machine::with('modality')
+                        ->get()
+                        ->countBy('modality.modality')
+                        ->sortDesc();
+
+        return [
+            'datasets' => [
+                [
+                    'data' => $machines->flatten(1)->all(),
+                ],
+            ],
+            'labels' => $machines->keys()->all(),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'pie';
+    }
+}

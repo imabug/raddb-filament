beee27201ec605fbd9a5a2c6726b76591e2394a1
Eugene Mah <eugenemah@gmail.com>
2025-09-16 09:15:37 -0400

    Add Status enum

---
+++ app/Enums/Status.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Enums;
+
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasIcon;
+use Filament\Support\Contracts\HasLabel;
+use Filament\Support\Icons\Heroicon;
+
+enum Status: string implements HasColor,HasIcon,HasLabel
+{
+    case Active = 'Active';
+    case Inactive = 'Inactive';
+    case Removed = 'Removed';
+
+    public function getColor(): string | array | null
+    {
+        return match ($this) {
+            self::Active => 'success',
+            self::Inactive => 'warning',
+            self::Removed => 'danger'
+        };
+    }
+
+    public function getIcon(): ?string
+    {
+        return match ($this) {
+            self::Active => 'heroicon-s-check',
+            self::Inactive => 'heroicon-s-x-circle',
+            self::Removed => 'heroicon-s-trash'
+        };
+    }
+
+    public function getLabel(): ?string
+    {
+        return match ($this) {
+            self::Active => "Active",
+            self::Inactive => "Inactive",
+            self::Removed => "Removed"
+        };
+    }
+}

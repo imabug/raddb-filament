0a5299cbab3b76f36699b123b88f68bd254bc303
Eugene Mah <eugenemah@gmail.com>
2025-10-15 18:12:15 -0400

    Preserve machine ID when creating another tube

--- app/Filament/Raddb/Resources/Tubes/Pages/CreateTube.php
+++ app/Filament/Raddb/Resources/Tubes/Pages/CreateTube.php
@@ -4,8 +4,14 @@ namespace App\Filament\Raddb\Resources\Tubes\Pages;
 
 use App\Filament\Raddb\Resources\Tubes\TubeResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Arr;
 
 class CreateTube extends CreateRecord
 {
     protected static string $resource = TubeResource::class;
+
+    protected function preserveFormDataWhenCreatingAnother(array $data): array
+    {
+        return Arr::only($data, ['machine_id']);
+    }
 }

ee5afd0e8b760a97b0e771178ad8e564b6cbd629
Eugene Mah <eugenemah@gmail.com>
2025-12-02 14:23:46 -0500

    Remove unused columns

--- app/Filament/Admin/Resources/Users/Schemas/UserInfolist.php
+++ app/Filament/Admin/Resources/Users/Schemas/UserInfolist.php
@@ -14,17 +14,8 @@ class UserInfolist
                 TextEntry::make('name'),
                 TextEntry::make('email')
                     ->label('Email address'),
-                TextEntry::make('email_verified_at')
-                    ->dateTime()
-                    ->placeholder('-'),
                 TextEntry::make('role')
                     ->badge(),
-                TextEntry::make('created_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('updated_at')
-                    ->dateTime()
-                    ->placeholder('-'),
             ]);
     }
 }

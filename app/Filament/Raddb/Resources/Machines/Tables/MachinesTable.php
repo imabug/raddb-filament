d08033213f89532189b91fcb9687c57ac82dc821
Eugene Mah <eugenemah@gmail.com>
2026-04-27 14:52:35 -0400

    Formatting

--- app/Filament/Raddb/Resources/Machines/Tables/MachinesTable.php
+++ app/Filament/Raddb/Resources/Machines/Tables/MachinesTable.php
@@ -23,71 +23,71 @@ class MachinesTable
     public static function configure(Table $table): Table
     {
         return $table
-            ->columns([
-                IconColumn::make('machine_status'),
-                TextColumn::make('location.location')
-                    ->searchable(),
-                TextColumn::make('modality.modality')
-                    ->searchable(),
-                TextColumn::make('description')
-                    ->searchable(),
-                TextColumn::make('manufacturer.manufacturer')
-                    ->searchable(),
-                TextColumn::make('model')
-                    ->searchable(),
-                TextColumn::make('serial_number')
-                    ->searchable(),
-                TextColumn::make('vend_site_id')
-                    ->searchable(),
-                TextColumn::make('room')
-                    ->searchable(),
-                TextColumn::make('manuf_date')
-                    ->date('Y-m-d')
-                    ->sortable(),
-                TextColumn::make('install_date')
-                    ->date('Y-m-d')
-                    ->sortable(),
-                TextColumn::make('remove_date')
-                    ->date('Y-m-d')
-                    ->sortable(),
-                TextColumn::make('age')
-                    ->sortable(),
-            ])
-            ->groups([
-                Group::make('facility.facility')
-                    ->collapsible(),
-                Group::make('location.location')
-                    ->collapsible(),
-                Group::make('modality.modality')
-                    ->collapsible(),
-                Group::make('manufacturer.manufacturer')
-                    ->collapsible(),
-            ])
-            ->defaultGroup('facility.facility')
-            ->filters([
-                TrashedFilter::make(),
-                Filter::make('active')
-                    ->query(fn(Builder $query): Builder => $query->where('machine_status', "Active"))
-                    ->toggle()
-                    ->default(),
-                SelectFilter::make('facility')
-                    ->relationship('facility', 'facility'),
-                SelectFilter::make('modality')
-                    ->relationship('modality', 'modality'),
-                SelectFilter::make('manufacturer')
-                    ->relationship('manufacturer', 'manufacturer'),
-            ])
-            ->deferFilters(false)
-            ->recordActions([
-                ViewAction::make(),
-                EditAction::make(),
-            ])
-            ->toolbarActions([
-                BulkActionGroup::make([
-                    DeleteBulkAction::make(),
-                    ForceDeleteBulkAction::make(),
-                    RestoreBulkAction::make(),
-                ]),
-            ]);
+                   ->columns([
+                       IconColumn::make('machine_status'),
+                       TextColumn::make('location.location')
+                           ->searchable(),
+                       TextColumn::make('modality.modality')
+                           ->searchable(),
+                       TextColumn::make('description')
+                           ->searchable(),
+                       TextColumn::make('manufacturer.manufacturer')
+                           ->searchable(),
+                       TextColumn::make('model')
+                           ->searchable(),
+                       TextColumn::make('serial_number')
+                           ->searchable(),
+                       TextColumn::make('vend_site_id')
+                           ->searchable(),
+                       TextColumn::make('room')
+                           ->searchable(),
+                       TextColumn::make('manuf_date')
+                           ->date('Y-m-d')
+                           ->sortable(),
+                       TextColumn::make('install_date')
+                           ->date('Y-m-d')
+                           ->sortable(),
+                       TextColumn::make('remove_date')
+                           ->date('Y-m-d')
+                           ->sortable(),
+                       TextColumn::make('age')
+                           ->sortable(),
+                   ])
+                   ->groups([
+                       Group::make('facility.facility')
+                           ->collapsible(),
+                       Group::make('location.location')
+                           ->collapsible(),
+                       Group::make('modality.modality')
+                           ->collapsible(),
+                       Group::make('manufacturer.manufacturer')
+                           ->collapsible(),
+                   ])
+                   ->defaultGroup('facility.facility')
+                   ->filters([
+                       TrashedFilter::make(),
+                       Filter::make('active')
+                           ->query(fn(Builder $query): Builder => $query->where('machine_status', "Active"))
+                           ->toggle()
+                           ->default(),
+                       SelectFilter::make('facility')
+                           ->relationship('facility', 'facility'),
+                       SelectFilter::make('modality')
+                           ->relationship('modality', 'modality'),
+                       SelectFilter::make('manufacturer')
+                           ->relationship('manufacturer', 'manufacturer'),
+                   ])
+                   ->deferFilters(false)
+                   ->recordActions([
+                       ViewAction::make(),
+                       EditAction::make(),
+                   ])
+                   ->toolbarActions([
+                       BulkActionGroup::make([
+                           DeleteBulkAction::make(),
+                           ForceDeleteBulkAction::make(),
+                           RestoreBulkAction::make(),
+                       ]),
+                   ]);
     }
 }

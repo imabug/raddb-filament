363672e97d7948a579185509b779e34013fb03d6
Eugene Mah <eugenemah@gmail.com>
2026-04-28 10:35:59 -0400

    Fix type relationship to properly populate survey type column.
    Fixes #18

--- app/Filament/Raddb/Resources/TestDates/Tables/TestDatesTable.php
+++ app/Filament/Raddb/Resources/TestDates/Tables/TestDatesTable.php
@@ -2,9 +2,10 @@
 
 namespace App\Filament\Raddb\Resources\TestDates\Tables;
 
+use App\Filament\Actions\TableEditAction;
+use App\Filament\Actions\TableViewAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
-use Filament\Actions\EditAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
 use Filament\Tables\Columns\TextColumn;
@@ -21,25 +22,26 @@ class TestDatesTable
             ->columns([
                 TextColumn::make('machine.description')
                     ->searchable(),
-                TextColumn::make('type.test_type')
+                TextColumn::make('testtype.test_type')
                     ->sortable(),
                 TextColumn::make('test_date')
                     ->date('Y-m-d')
                     ->sortable(),
                 TextColumn::make('accession')
                     ->searchable(),
-                TextColumn::make('notes')
+                TextColumn::make('notes'),
             ])
             ->filters([
                 TrashedFilter::make(),
                 Filter::make('pending')
-                    ->query(fn (Builder $query): Builder => $query->where('test_date', '>=', date('Y-m-d')))
+                    ->query(fn(Builder $query): Builder => $query->where('test_date', '>=', date('Y-m-d')))
                     ->toggle()
                     ->default(),
             ])
             ->deferFilters(false)
             ->recordActions([
-                EditAction::make(),
+                TableEditAction::make(),
+                TableViewAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([

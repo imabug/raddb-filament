b0fc58bef6b7a75d8d1e0f87507298a3c9bd254f
Eugene Mah <eugenemah@gmail.com>
2026-05-07 13:18:12 -0400

    Add navigation items

--- app/Providers/Filament/ShieldingPanelProvider.php
+++ app/Providers/Filament/ShieldingPanelProvider.php
@@ -2,15 +2,15 @@
 
 namespace App\Providers\Filament;
 
-use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationItem;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets\AccountWidget;
+use Filament\Support\Icons\Heroicon;
 use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -24,34 +24,44 @@ class ShieldingPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->id('shielding')
-            ->path('shielding')
-            ->colors([
-                'primary' => Color::Amber,
-            ])
-            ->discoverResources(in: app_path('Filament/Shielding/Resources'), for: 'App\Filament\Shielding\Resources')
-            ->discoverPages(in: app_path('Filament/Shielding/Pages'), for: 'App\Filament\Shielding\Pages')
-            ->pages([
-                Dashboard::class,
-            ])
-            ->discoverWidgets(in: app_path('Filament/Shielding/Widgets'), for: 'App\Filament\Shielding\Widgets')
-            ->widgets([
-                AccountWidget::class,
-                FilamentInfoWidget::class,
-            ])
-            ->middleware([
-                EncryptCookies::class,
-                AddQueuedCookiesToResponse::class,
-                StartSession::class,
-                AuthenticateSession::class,
-                ShareErrorsFromSession::class,
-                PreventRequestForgery::class,
-                SubstituteBindings::class,
-                DisableBladeIconComponents::class,
-                DispatchServingFilamentEvent::class,
-            ])
-            ->authMiddleware([
-                Authenticate::class,
-            ]);
+                   ->id('shielding')
+                   ->path('shielding')
+                   ->colors([
+                       'primary' => Color::Amber,
+                   ])
+                   ->discoverResources(in: app_path('Filament/Shielding/Resources'), for: 'App\Filament\Shielding\Resources')
+                   ->discoverPages(in: app_path('Filament/Shielding/Pages'), for: 'App\Filament\Shielding\Pages')
+                   ->pages([
+                       Dashboard::class,
+                   ])
+                   ->discoverWidgets(in: app_path('Filament/Shielding/Widgets'), for: 'App\Filament\Shielding\Widgets')
+                   ->widgets([
+                   ])
+                   ->navigationItems([
+                       NavigationItem::make('RadDB Dashboard')
+                           ->url('/')
+                           ->icon(Heroicon::OutlinedPresentationChartBar)
+                           ->sort(1),
+                       NavigationItem::make('Survey Schedule')
+                           ->url('/raddb/survey-schedule-views')
+                           ->icon(Heroicon::OutlinedHome)
+                           ->sort(1),
+                       NavigationItem::make('RadDB')
+                           ->url('/raddb')
+                           ->icon(Heroicon::OutlinedHome)
+                           ->sort(1),
+                   ])
+                   ->middleware([
+                       EncryptCookies::class,
+                       AddQueuedCookiesToResponse::class,
+                       StartSession::class,
+                       AuthenticateSession::class,
+                       ShareErrorsFromSession::class,
+                       PreventRequestForgery::class,
+                       SubstituteBindings::class,
+                       DisableBladeIconComponents::class,
+                       DispatchServingFilamentEvent::class,
+                   ])
+                   ->authMiddleware([]);
     }
 }

817c5d99130581cc7fb62b76b8a6bc3dd55e885d
Eugene Mah <eugenemah@gmail.com>
2026-05-07 13:18:38 -0400

    Migration for shielding requests table

---
+++ database/migrations/2026_05_07_155806_shielding_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        /*
+         * Table to manage shielding plan requests
+         */
+        Schema::create('shielding_requests', function (Blueprint $table) {
+            $table->id();
+            $table->tinyText('description')
+                ->nullable(false)
+                ->index();
+            $table->date('request_date')
+                ->nullable(false)
+                ->index();
+            $table->foreignId('user_id')
+                ->comment('Foreign key to users table')
+                ->nullable(false)
+                ->index()
+                ->constrained(table: 'users')
+                ->noActionOnUpdate()
+                ->noActionOnDelete();
+            $table->string('status', 20)
+                ->nullable(false)
+                ->index();
+            $table->date('completion_date')
+                ->nullable()
+                ->index();
+            $table->text('notes')->nullable()->fullText();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};

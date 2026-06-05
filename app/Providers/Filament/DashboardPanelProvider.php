61e33a68babf498e75195de655de42047de0246b
Eugene Mah <eugenemah@gmail.com>
2026-06-05 11:05:05 -0400

    Adjust indenting

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -23,49 +23,49 @@ class DashboardPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
-                   ->default()
-                   ->id('dashboard')
-                   ->path('')
-            ->viteTheme('resources/css/filament/dashboard/theme.css')
-                   ->brandName('RadDB Filament')
-                   // ->login()
-                   ->colors([
-                       'primary' => Color::Amber,
-                   ])
-                   ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
-                   ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
-                   ->pages([
-                       Dashboard::class,
-                   ])
-                   ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
-                   ->widgets([
-                   ])
-                   ->navigationItems([
-                       NavigationItem::make('Survey Schedule')
-                           ->url('/raddb/survey-schedule-views')
-                           ->icon(Heroicon::OutlinedHome)
-                           ->sort(1),
-                       NavigationItem::make('RadDB')
-                           ->url('/raddb')
-                           ->icon(Heroicon::OutlinedHome)
-                           ->sort(1),
-                       NavigationItem::make('Shielding Requests')
-                           ->url('/shielding')
-                           ->icon(Heroicon::OutlinedShieldCheck)
-                           ->sort(1),
-                   ])
-                   ->middleware([
-                       EncryptCookies::class,
-                       AddQueuedCookiesToResponse::class,
-                       StartSession::class,
-                       AuthenticateSession::class,
-                       ShareErrorsFromSession::class,
-                       PreventRequestForgery::class,
-                       SubstituteBindings::class,
-                       DisableBladeIconComponents::class,
-                       DispatchServingFilamentEvent::class,
-                   ])
-                   ->authMiddleware([
-                   ]);
+                ->default()
+                ->id('dashboard')
+                ->path('')
+                ->viteTheme('resources/css/filament/dashboard/theme.css')
+                ->brandName('RadDB Filament')
+                // ->login()
+                ->colors([
+                    'primary' => Color::Amber,
+                ])
+                ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
+                ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
+                ->pages([
+                    Dashboard::class,
+                ])
+                ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
+                ->widgets([
+                ])
+                ->navigationItems([
+                    NavigationItem::make('Survey Schedule')
+                        ->url('/raddb/survey-schedule-views')
+                        ->icon(Heroicon::OutlinedHome)
+                        ->sort(1),
+                    NavigationItem::make('RadDB')
+                        ->url('/raddb')
+                        ->icon(Heroicon::OutlinedHome)
+                        ->sort(1),
+                    NavigationItem::make('Shielding Requests')
+                        ->url('/shielding')
+                        ->icon(Heroicon::OutlinedShieldCheck)
+                        ->sort(1),
+                ])
+                ->middleware([
+                    EncryptCookies::class,
+                    AddQueuedCookiesToResponse::class,
+                    StartSession::class,
+                    AuthenticateSession::class,
+                    ShareErrorsFromSession::class,
+                    PreventRequestForgery::class,
+                    SubstituteBindings::class,
+                    DisableBladeIconComponents::class,
+                    DispatchServingFilamentEvent::class,
+                ])
+                ->authMiddleware([
+                ]);
     }
 }
